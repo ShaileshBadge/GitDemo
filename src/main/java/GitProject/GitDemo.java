3e095f2b6f3ea0c5e096ec2b5f5ebc22df2a083d
Badge47S <Badge47S@PUN-MH-D-148032.inroot.in>
2021-05-27 19:30:37 +0530

    First Commit.....

---
+++ src/main/java/GitProject/GitDemo.java
@@ -0,0 +1,14 @@
+package GitProject;
+import org.testng.annotations.Test;
+public class GitDemo {
+	@Test
+	public void test() {
+		String Atul = "Vergin";
+		if (Atul == "Vergin") {
+			System.out.println("Search GF & Oyo");
+		}
+		else {
+			System.out.println("It is impossible");
+		}
+	}
+}
